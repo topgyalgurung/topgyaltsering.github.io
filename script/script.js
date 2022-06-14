9dbf64a15c34eeb750de782c306a9e5dcaf89799
Topgyal Gurung <topgyaltsering3@gmail.com>
2022-06-13 22:45:59 -0400

    remove card

--- script/script.js
+++ script/script.js
@@ -25,3 +25,12 @@ navLinks.forEach((l) => {
         x.style.display = "block";
       }
     }
+/* Open when someone clicks on the span element */
+function openNav() {
+    document.getElementById("myNav").style.width = "100%";
+  }
+  
+  /* Close when someone clicks on the "x" symbol inside the overlay */
+  function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+  }
